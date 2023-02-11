c25b96a51654a327a41795ead73bc9d051fdf863
Neha Dahal <nehudahal57@gmail.com>
2023-02-12 00:04:05 +0545

    bug fixes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const Cashflows = require("../models/Cashflows");
 const Restaurants = require("../models/Restaurants");
 const Orphanages = require("../models/Orphanages");
 const Donations = require("../models/Donations");
@@ -55,45 +54,10 @@ router.post("/save-donations", async function (req, res, next) {
   });
   res.redirect("/newhome");
 });
-// router.post("/save-cashflows", async function (req, res, next) {
-//   await Cashflows.insertMany({
-//     //console.log(req.body);
-//     date: req.body.date,
-//     category: req.body.category,
-//     amount: req.body.amount,
-//   });
-//   res.redirect("/history");
-// });
 router.get("/dashboard", async function (req, res, next) {
   const donations = await Donations.find();
   console.log(donations);
   res.render("dashboard", { donationList: donations });
 });
-// router.get("/reports", async function (req, res, next) {
-//   currentDate = new Date();
-//   startDate = new Date(currentDate.getFullYear(), 0, 1);
-//   var days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
 
-//   var weekNumber = Math.ceil(days / 7);
-//   var monthNumber = new Date().getMonth() + 1;
-//   console.log("Month number=", monthNumber);
-//   const cashflows = await Cashflows.aggregate([
-//     {
-//       $group: { _id: { $week: "$date" }, weekly_total: { $sum: "$amount" } },
-//     },
-//     { $match: { _id: weekNumber } },
-//   ]);
-//   //console.log(cashflows);
-
-// const cashflows2 = await Cashflows.aggregate([
-//   {
-//     $group: {
-//       _id: { $month: "$date" },
-//       monthly_total: { $sum: "$amount" },
-//     },
-//   },
-//   { $match: { _id: monthNumber } },
-// ]);
-//   res.render("reports", { cashflowList: cashflows, cashflowList2: cashflows2 });
-// });
 module.exports = router;
